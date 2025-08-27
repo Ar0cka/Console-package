39ba0b5acf3218f8001681642eb10730f9275cb5
Ar0cka <vanoshul@gmail.com>
2025-08-27 20:41:16 +1200

    Update init and package info

---
+++ ConsolePackage/Runtime/Scripts/ConsoleAttribute/ConsoleCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp.Runtime.ConsoleAttribute
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ConsoleCommand : Attribute
+    {
+        public string Command { get; }
+
+        public ConsoleCommand(string command)
+        {
+            Command = command;
+        }
+    }
+}
---
+++ ConsolePackage/Runtime/Scripts/Initialize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using ConsoleApp.Runtime.ConsoleAttribute;
+using UnityEngine;
+
+namespace ConsoleApp.Runtime
+{
+    [DefaultExecutionOrder(-100)]
+    public class Initialize : MonoBehaviour
+    {
+        [SerializeField] private string path;
+        [SerializeField] private ConsoleUI consoleUI;
+        
+        private void Awake()
+        {
+            path = !string.IsNullOrEmpty(path) ? path : CreateFileInDocuments();
+            
+            ConsoleLogger.InitializeLogs(new Logger(path));
+            
+            consoleUI.Initialize();
+            
+            CommandRegister.Register(typeof(Initialize).Assembly);
+            
+            ConsoleLogger.Info("Starting ConsoleApp");
+        }
+
+        private string CreateFileInDocuments()
+        {
+            string pathToDocuments = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string fullPath = Path.Combine(pathToDocuments, "Log.txt");
+            
+            if (!File.Exists(fullPath))
+            {
+                File.WriteAllText(fullPath, "Файл создан автоматически");
+            }
+            
+            File.WriteAllText(fullPath, "");
+            
+            return fullPath;
+        }
+    }
+}
